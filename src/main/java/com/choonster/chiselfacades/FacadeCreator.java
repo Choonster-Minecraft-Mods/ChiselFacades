ad5881e8158b369cb5311bbca75e3a3a0e13c89e
Choonster <Choonster.2010@gmail.com>
2015-01-16 22:09:43 +1100

    Update to Chisel 2.2.2.0a6e6fc (dev version)
    - Rewrite FacadeCreator.init around the ICarvingGroup API
    - Send ItemStack IMC messages instead of Strings
    - Separate Client and Server debug output files
    - Add block.toString() to debug output
    - Remove Colored Sand Facades (block removed from Chisel)
    - Add Facades for the following blocks:
    -- AntiBlock
    -- All Road Line variations
    -- Tallow
    -- Thaumium
    -- Torch
    -- Warning Sign

--- src/main/java/com/choonster/chiselfacades/FacadeCreator.java
+++ src/main/java/com/choonster/chiselfacades/FacadeCreator.java
@@ -1,146 +1,160 @@
 package com.choonster.chiselfacades;
 
-import com.cricketcraft.chisel.api.ICarvable;
-import com.cricketcraft.chisel.block.BlockSnakestone;
-import com.cricketcraft.chisel.carving.CarvableVariation;
+import com.cricketcraft.chisel.Features;
+import com.cricketcraft.chisel.api.carving.ICarvingGroup;
+import com.cricketcraft.chisel.api.carving.ICarvingVariation;
+import com.cricketcraft.chisel.carving.Carving;
 import com.cricketcraft.chisel.config.Configurations;
 import com.cricketcraft.chisel.init.ChiselBlocks;
-import com.cricketcraft.chisel.init.Features;
+import com.cricketcraft.chisel.utils.General;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLInterModComms;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 
 import java.io.PrintWriter;
 
+
 public class FacadeCreator {
 	private static int _numFacades = 0;
 	private static PrintWriter writer;
 
 	private static void sendFacadeMessage(Block block, int meta) {
-		String blockName = Block.blockRegistry.getNameForObject(block);
+		ItemStack stack = new ItemStack(block, 1, meta);
 
-		FMLInterModComms.sendMessage("BuildCraft|Transport",
-				"add-facade", blockName + "@" + meta);
+		FMLInterModComms.sendMessage("BuildCraft|Transport", "add-facade", stack);
 
 		if (writer != null) {
-			writer.printf("%s - %d - %s\n", blockName, meta, new ItemStack(block, 1, meta).getHasSubtypes());
+			String blockName = Block.blockRegistry.getNameForObject(block);
+			writer.printf("%s - %s - %d - %d - %s\n", blockName, block.toString(), block.getRenderType(), meta, stack.getHasSubtypes());
 		}
 
 		_numFacades++;
 	}
 
-	private static <T extends Block & ICarvable> void registerFacade(T block) {
-		boolean hasVariations = false;
+	private static void registerGroup(String groupName) {
+		ICarvingGroup group = Carving.chisel.getGroup(groupName);
 
-		for (int meta = 0; meta <= 15; meta++) {
-			CarvableVariation variation = block.getVariation(meta);
-			if (variation != null) {
-				hasVariations = true;
-				sendFacadeMessage(block, meta);
+		if (group == null) {
+			Logger.warn("CarvingGroup %s is null!", groupName);
+			return;
+		}
+
+		for (ICarvingVariation variation : group.getVariations()) {
+			Block block = variation.getBlock();
+			if (Block.blockRegistry.getNameForObject(block).startsWith("chisel:")) { // Ignore non-Chisel blocks
+				sendFacadeMessage(block, variation.getItemMeta());
 			}
 		}
-
-		if (!hasVariations) {
-			Logger.warn("Block has no variations. %s - %s", block.getUnlocalizedName(), block);
-		}
 	}
 
-	private static void registerFacadeIfEnabled(Features feature, BlockSnakestone block) {
+	private static void registerGroupIfEnabled(Features feature, String groupName) {
 		if (feature.enabled()) {
-			sendFacadeMessage(block, Constants.SNAKESTONE_HEAD_META);
-			sendFacadeMessage(block, Constants.SNAKESTONE_BODY_META);
-		}
-	}
-
-	private static <T extends Block & ICarvable> void registerFacadeIfEnabled(Features feature, T block) {
-		if (feature.enabled()) {
-			registerFacade(block);
-		}
-	}
-
-	private static <T extends Block & ICarvable> void registerFacadeIfEnabled(Features feature, T[] blocks) {
-		if (feature.enabled()) {
-			for (int i = 0; i < blocks.length; i++) {
-				registerFacade(blocks[i]);
-			}
+			registerGroup(groupName);
 		}
 	}
 
 	public static int init() {
 		if (Config.debugOutputEnabled) {
 			try {
-				writer = new PrintWriter("ChiselFacadesDebug.txt", "UTF-8");
-				writer.println("Name - Meta - Subtypes");
+				writer = new PrintWriter("ChiselFacades" + (FMLCommonHandler.instance().getEffectiveSide().isClient() ? "Client" : "Server") + "Debug.txt", "UTF-8");
+				writer.println("Name - Instance - RenderType - Meta - Subtypes");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 
+		String[] stainedGlassNames = new String[16];
+		for (int i = 0; i < 16; i++) {
+			stainedGlassNames[i] = General.sGNames[i].replaceAll(" ", "").toLowerCase();
+		}
+
 		// Ignore AE Certus Quartz and Sky Stone because Chisel doesn't add new variations,
 		// it only cycles through existing ones
 
-		registerFacadeIfEnabled(Features.ANDESITE, ChiselBlocks.andesite);
+		// Ignore Thaumcraft Amber because Chisel doesn't add new variations,
+		// it only cycles through existing ones
 
-		registerFacadeIfEnabled(Features.ARCANE, ChiselBlocks.arcane);
+		registerGroupIfEnabled(Features.ANDESITE, "andesite");
+
+		registerGroupIfEnabled(Features.ANTIBLOCK, "antiBlock");
+
+		registerGroupIfEnabled(Features.ARCANE, "arcane");
 
 		// Ignore Auto Chisel, Auto Chisel Upgrades and Ball of Moss because
 		// BC doesn't allow Facades of TileEntity blocks (i.e. Auto Chisel) and the other two are only items
 
-		registerFacadeIfEnabled(Features.BLOOD_RUNE, ChiselBlocks.bloodRune);
+		registerGroupIfEnabled(Features.BLOOD_RUNE, "bloodRune");
 
-		registerFacadeIfEnabled(Features.BOOKSHELF, ChiselBlocks.bookshelf);
+		// Ignore Blood Magic Blood Block because Chisel doesn't add new variations,
+		// it only cycles through existing ones
 
-		registerFacadeIfEnabled(Features.BRICK_CUSTOM, ChiselBlocks.brickCustom);
+		registerGroupIfEnabled(Features.BOOKSHELF, "bookshelf");
 
-		registerFacadeIfEnabled(Features.CARPET, ChiselBlocks.carpet_block);
+		registerGroupIfEnabled(Features.BRICK_CUSTOM, "brickCustom");
 
-		// Ignore Carpet Floor because it uses the same textures as the full block version
+		// Carpets can't be chiseled, so they don't have a ICarvingGroup to register
+		if (Features.CARPET.enabled()) {
+			for (int meta = 0; meta < 16; meta++) {
+				sendFacadeMessage(ChiselBlocks.carpet_block, meta);
+			}
+		}
 
-		registerFacadeIfEnabled(Features.CLOUD, ChiselBlocks.cloud);
+		// Ignore Floor Carpets because they use the same textures as the full block version
 
-		registerFacadeIfEnabled(Features.COBBLESTONE, ChiselBlocks.cobblestone);
+		// Ignore Chisel becuase it only adds items
 
-		registerFacadeIfEnabled(Features.COBBLESTONE_MOSSY, ChiselBlocks.mossy_cobblestone);
+		registerGroupIfEnabled(Features.CLOUD, "cloud");
 
-		registerFacadeIfEnabled(Features.COLORED_SAND, ChiselBlocks.colored_sand);
+		registerGroupIfEnabled(Features.COBBLESTONE, "cobblestone");
 
-		registerFacadeIfEnabled(Features.CONCRETE, ChiselBlocks.concrete);
+		registerGroupIfEnabled(Features.COBBLESTONE_MOSSY, "mossy_cobblestone");
 
-		registerFacadeIfEnabled(Features.DIAMOND_BLOCK, ChiselBlocks.diamond_block);
+		registerGroupIfEnabled(Features.CONCRETE, "concrete");
 
-		registerFacadeIfEnabled(Features.DIORITE, ChiselBlocks.diorite);
+		registerGroupIfEnabled(Features.DIAMOND_BLOCK, "diamond_block");
 
-		registerFacadeIfEnabled(Features.DIRT, ChiselBlocks.dirt);
+		registerGroupIfEnabled(Features.DIORITE, "diorite");
 
-		registerFacadeIfEnabled(Features.EMERALD_BLOCK, ChiselBlocks.emerald_block);
+		registerGroupIfEnabled(Features.DIRT, "dirt");
 
-		registerFacadeIfEnabled(Features.FACTORY, ChiselBlocks.factoryblock);
+		registerGroupIfEnabled(Features.EMERALD_BLOCK, "emerald_block");
 
-		registerFacadeIfEnabled(Features.FANTASY, ChiselBlocks.fantasyblock);
+		registerGroupIfEnabled(Features.FACTORY, "factoryblock");
 
-		registerFacadeIfEnabled(Features.FANTASY2, ChiselBlocks.fantasyblock2);
+		registerGroupIfEnabled(Features.FANTASY, "fantasyblock");
 
-		registerFacadeIfEnabled(Features.FUTURA, ChiselBlocks.futura);
+		registerGroupIfEnabled(Features.FANTASY, "fantasyblock2");
 
-		registerFacadeIfEnabled(Features.GLASS, ChiselBlocks.glass);
+		registerGroupIfEnabled(Features.FUTURA, "futura");
 
-		registerFacadeIfEnabled(Features.GLASS_PANE, ChiselBlocks.glass_pane);
+		registerGroupIfEnabled(Features.GLASS, "glass");
 
-		registerFacadeIfEnabled(Features.GLASS_STAINED, ChiselBlocks.stainedGlass);
+		registerGroupIfEnabled(Features.GLASS_PANE, "glass_pane");
 
-		registerFacadeIfEnabled(Features.GLASS_STAINED_PANE, ChiselBlocks.stainedGlassPane);
+		if (Features.GLASS_STAINED.enabled()) {
+			for (int i = 0; i < 16; i++) {
+				registerGroup("stained_glass_" + stainedGlassNames[i]);
+			}
+		}
 
-		registerFacadeIfEnabled(Features.GLOWSTONE, ChiselBlocks.glowstone);
+		if (Features.GLASS_STAINED_PANE.enabled()) {
+			for (int i = 0; i < 16; i++) {
+				registerGroup("stained_glass_pane_" + stainedGlassNames[i]);
+			}
+		}
 
-		registerFacadeIfEnabled(Features.GOLD_BLOCK, ChiselBlocks.gold_block);
+		registerGroupIfEnabled(Features.GLOWSTONE, "glowstone");
 
-		registerFacadeIfEnabled(Features.GRANITE, ChiselBlocks.granite);
+		registerGroupIfEnabled(Features.GOLD_BLOCK, "gold_block");
 
-		registerFacadeIfEnabled(Features.GRIMSTONE, ChiselBlocks.grimstone);
+		registerGroupIfEnabled(Features.GRANITE, "granite");
 
-		registerFacadeIfEnabled(Features.HEX_PLATING, ChiselBlocks.hexPlating);
+		registerGroupIfEnabled(Features.GRIMSTONE, "grimstone");
 
-		registerFacadeIfEnabled(Features.HOLYSTONE, ChiselBlocks.holystone);
+		registerGroupIfEnabled(Features.HEX_PLATING, "hexPlating");
+
+		registerGroupIfEnabled(Features.HOLYSTONE, "holystone");
 
 		// Can't register Ice or Ice Pillars because they have getRenderType() == 0.
 		// BuildCraft tries to add Facades for blocks with this renderType on its own, but it doesn't work properly with
@@ -148,51 +162,51 @@ public class FacadeCreator {
 
 		// Ignore Ice Stairs because they use the same textures as the full block version
 
-		registerFacadeIfEnabled(Features.IRON_BARS, ChiselBlocks.iron_bars);
+		registerGroupIfEnabled(Features.IRON_BARS, "iron_bars");
 
-		registerFacadeIfEnabled(Features.IRON_BLOCK, ChiselBlocks.iron_block);
+		registerGroupIfEnabled(Features.IRON_BLOCK, "iron_block");
 
 		// Ignore Jack o'Lanterns because BuildCraft registers them itself
 
-		registerFacadeIfEnabled(Features.LABORATORY, ChiselBlocks.laboratoryblock);
+		registerGroupIfEnabled(Features.LABORATORY, "laboratoryblock");
 
-		registerFacadeIfEnabled(Features.LAPIS_BLOCK, ChiselBlocks.lapis_block);
+		registerGroupIfEnabled(Features.LAPIS_BLOCK, "lapis_block");
 
-		registerFacadeIfEnabled(Features.LAVASTONE, ChiselBlocks.lavastone);
+		registerGroupIfEnabled(Features.LAVASTONE, "lavastone");
 
-		registerFacadeIfEnabled(Features.LEAVES, ChiselBlocks.leaves);
+		registerGroupIfEnabled(Features.LEAVES, "leaves");
 
-		registerFacadeIfEnabled(Features.LIMESTONE, ChiselBlocks.limestone);
+		registerGroupIfEnabled(Features.LIMESTONE, "limestone");
 
 		// Ignore Limestone Slabs and Stairs because they use the same textures as the full block version
 
-		registerFacadeIfEnabled(Features.MARBLE, ChiselBlocks.marble);
+		registerGroupIfEnabled(Features.MARBLE, "marble");
 
-		registerFacadeIfEnabled(Features.MARBLE, ChiselBlocks.marble_slab);
+		registerGroupIfEnabled(Features.MARBLE, "marble_slab");
 
 		// Ignore Marble Stairs because they use the same textures as the slab version
 
 		if (Features.MARBLE_PILLAR.enabled()) {
 			if (Configurations.oldPillars) {
 				// Old Pillar blocks work, old Pillar slabs are broken
-				registerFacade(ChiselBlocks.marble_pillar);
+				registerGroup("marble_pillar");
 				Logger.info("Old pillars enabled, adding Pillar block Facades");
 			} else {
 				// New Pillar blocks are broken, new Pillar slabs work
-				registerFacade(ChiselBlocks.marble_pillar_slab);
+				registerGroup("marble_pillar_slab");
 				Logger.info("Old pillars disabled, adding Pillar slab Facades");
 			}
 		}
 
-		registerFacadeIfEnabled(Features.NETHER_BRICK, ChiselBlocks.nether_brick);
+		registerGroupIfEnabled(Features.NETHER_BRICK, "nether_brick");
 
-		registerFacadeIfEnabled(Features.NETHER_RACK, ChiselBlocks.netherrack);
+		registerGroupIfEnabled(Features.NETHER_RACK, "netherrack");
 
-		registerFacadeIfEnabled(Features.OBSIDIAN, ChiselBlocks.obsidian);
+		registerGroupIfEnabled(Features.OBSIDIAN, "obsidian");
 
 		// Can't register Packed Ice blocks or pillars due to their renderType (see reasoning for regular Ice).
 
-		registerFacadeIfEnabled(Features.PAPER_WALL, ChiselBlocks.paperwall);
+		registerGroupIfEnabled(Features.PAPER_WALL, "paperwall");
 
 		// Ignore Presents because they only show the vanilla Oak Wood Planks texture as Facades
 
@@ -202,48 +216,60 @@ public class FacadeCreator {
 
 		// Ignore Railcraft blocks because Chisel doesn't add new variations, it only cycles through existing ones
 
-		registerFacadeIfEnabled(Features.REDSTONE_BLOCK, ChiselBlocks.redstone_block);
+		registerGroupIfEnabled(Features.REDSTONE_BLOCK, "redstone_block");
 
-		if (Features.ROAD_LINE.enabled()) {
-			// Looks weird because it's not a full block, but it doesn't look broken
-			sendFacadeMessage(ChiselBlocks.road_line, 0);
-		}
+		// Looks weird because it's not a full block, but it doesn't look broken
+		registerGroupIfEnabled(Features.ROAD_LINE, "road_line");
 
-		registerFacadeIfEnabled(Features.SANDSTONE, ChiselBlocks.sandstone);
+		registerGroupIfEnabled(Features.SANDSTONE, "sandstone");
 
-		registerFacadeIfEnabled(Features.SANDSTONE_SCRIBBLES, ChiselBlocks.sandstone_scribbles);
+		registerGroupIfEnabled(Features.SANDSTONE_SCRIBBLES, "sandstone_scribbles");
 
 		// Ignore Smashing Rock because it's not a block
 
-		registerFacadeIfEnabled(Features.SNAKE_SANDSTONE, ChiselBlocks.sand_snakestone);
+		// Sand Snakestone is registered with the "sandstone" group
 
-		registerFacadeIfEnabled(Features.SNAKESTONE, ChiselBlocks.stone_snakestone);
+		registerGroupIfEnabled(Features.SNAKESTONE, "stonebrick");
 
-		registerFacadeIfEnabled(Features.SNAKESTONE_OBSIDIAN, ChiselBlocks.obsidian_snakestone);
+		// Obsidian Snakestone is registered with the "obsidian" group
 
-		registerFacadeIfEnabled(Features.STONE_BRICK, ChiselBlocks.stonebricksmooth);
+		registerGroupIfEnabled(Features.STONE_BRICK, "stonebricksmooth");
 
-		registerFacadeIfEnabled(Features.TECHNICAL, ChiselBlocks.technical);
-		registerFacadeIfEnabled(Features.TECHNICAL, ChiselBlocks.technical2);
+		registerGroupIfEnabled(Features.TALLOW, "tallow");
 
-		registerFacadeIfEnabled(Features.TEMPLE_BLOCK, ChiselBlocks.templeblock);
+		registerGroupIfEnabled(Features.TECHNICAL, "technical");
+		registerGroupIfEnabled(Features.TECHNICAL, "technical2");
 
-		registerFacadeIfEnabled(Features.TEMPLE_BLOCK_MOSSY, ChiselBlocks.mossy_templeblock);
+		registerGroupIfEnabled(Features.TEMPLE_BLOCK, "templeblock");
+
+		registerGroupIfEnabled(Features.TEMPLE_BLOCK_MOSSY, "mossy_templeblock");
 
 		// Ignore Twilight Forest blocks because Chisel doesn't add new variations, it only cycles through existing ones
 
-		registerFacadeIfEnabled(Features.TYRIAN, ChiselBlocks.tyrian);
+		registerGroupIfEnabled(Features.THAUMIUM, "thaumium");
 
-		registerFacadeIfEnabled(Features.VOIDSTONE, ChiselBlocks.voidstone);
-		registerFacadeIfEnabled(Features.VOIDSTONE, ChiselBlocks.voidstone2);
+		// Looks weird because it's not a full block, but it doesn't look broken
+		registerGroupIfEnabled(Features.TORCH, "torch");
 
-		// Can't register Voidstone Pillars due to their renderType (see reasoning for Ice).
+		registerGroupIfEnabled(Features.TYRIAN, "tyrian");
 
-		registerFacadeIfEnabled(Features.WATERSTONE, ChiselBlocks.waterstone);
+		registerGroupIfEnabled(Features.VOIDSTONE, "voidstone");
+		registerGroupIfEnabled(Features.VOIDSTONE, "voidstone2");
 
-		registerFacadeIfEnabled(Features.WOOD, ChiselBlocks.planks);
+		// Ignore Voidstone Pillars because they display the purple and black missing texture icon as Facades
 
-		registerFacadeIfEnabled(Features.WOOLEN_CLAY, ChiselBlocks.woolen_clay);
+		registerGroupIfEnabled(Features.WARNING_SIGN, "warningSign");
+
+		registerGroupIfEnabled(Features.WATERSTONE, "waterstone");
+
+		if (Features.WOOD.enabled()) {
+			String[] plank_names = {"oak", "spruce", "birch", "jungle", "acacia", "dark_oak"};
+			for (int i = 0; i < plank_names.length; i++) {
+				registerGroup(plank_names[i] + "_planks");
+			}
+		}
+
+		registerGroupIfEnabled(Features.WOOLEN_CLAY, "woolen_clay");
 
 		if (writer != null) {
 			writer.close();
